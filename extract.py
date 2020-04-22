07eee7dfda136b9cdd51a5d7f0761bce67f2d1e8
jeongyw12382 <jeongyw12382@postech.ac.kr>
2020-04-22 07:27:29 -0400

    modified

--- extract.py
+++ extract.py
@@ -58,53 +58,63 @@ def extract_multiscale( net, img, detector, scale_f=2**0.25,
     B, three, H, W = img.shape
     assert B == 1 and three == 3, "should be a batch with a single RGB image"
     
-    assert max_scale <= 1
-    s = 1.0 # current scale factor
+    with torch.no_grad():
+        res = net(imgs=[img])
+
+    # assert max_scale <= 1
+    # s = 1.0 # current scale factor
     
-    X,Y,S,C,Q,D = [],[],[],[],[],[]
-    while  s+0.001 >= max(min_scale, min_size / max(H,W)):
-        if s-0.001 <= min(max_scale, max_size / max(H,W)):
-            nh, nw = img.shape[2:]
-            if verbose: print(f"extracting at scale x{s:.02f} = {nw:4d}x{nh:3d}")
-            # extract descriptors
-            with torch.no_grad():
-                res = net(imgs=[img])
+    # X,Y,S,C,Q,D = [],[],[],[],[],[]
+    # while  s+0.001 >= max(min_scale, min_size / max(H,W)):
+    #     if s-0.001 <= min(max_scale, max_size / max(H,W)):
+    #         nh, nw = img.shape[2:]
+    #         if verbose: print(f"extracting at scale x{s:.02f} = {nw:4d}x{nh:3d}")
+    #         # extract descriptors
+    #         with torch.no_grad():
+    #             res = net(imgs=[img])
                 
-            # get output and reliability map
-            descriptors = res['descriptors'][0]
-            reliability = res['reliability'][0]
-            repeatability = res['repeatability'][0]
+    #         # get output and reliability map
+    #         descriptors = res['descriptors'][0]
+    #         reliability = res['reliability'][0]
+    #         repeatability = res['repeatability'][0]
 
-            # normalize the reliability for nms
-            # extract maxima and descs
-            y,x = detector(**res) # nms
-            c = reliability[0,0,y,x]
-            q = repeatability[0,0,y,x]
-            d = descriptors[0,:,y,x].t()
-            n = d.shape[0]
+    #         # normalize the reliability for nms
+    #         # extract maxima and descs
+    #         y,x = detector(**res) # nms
+    #         c = reliability[0,0,y,x]
+    #         q = repeatability[0,0,y,x]
+    #         d = descriptors[0,:,y,x].t()
+    #         n = d.shape[0]
 
-            # accumulate multiple scales
-            X.append(x.float() * W/nw)
-            Y.append(y.float() * H/nh)
-            S.append((32/s) * torch.ones(n, dtype=torch.float32, device=d.device))
-            C.append(c)
-            Q.append(q)
-            D.append(d)
-        s /= scale_f
+    #         # accumulate multiple scales
+    #         X.append(x.float() * W/nw)
+    #         Y.append(y.float() * H/nh)
+    #         S.append((32/s) * torch.ones(n, dtype=torch.float32, device=d.device))
+    #         C.append(c)
+    #         Q.append(q)
+    #         D.append(d)
+    #     s /= scale_f
 
-        # down-scale the image for next iteration
-        nh, nw = round(H*s), round(W*s)
-        img = F.interpolate(img, (nh,nw), mode='bilinear', align_corners=False)
+    #     # down-scale the image for next iteration
+    #     nh, nw = round(H*s), round(W*s)
+    #     img = F.interpolate(img, (nh,nw), mode='bilinear', align_corners=False)
 
-    # restore value
-    torch.backends.cudnn.benchmark = old_bm
+    # # restore value
+    # torch.backends.cudnn.benchmark = old_bm
 
-    Y = torch.cat(Y)
-    X = torch.cat(X)
-    S = torch.cat(S) # scale
-    scores = torch.cat(C) * torch.cat(Q) # scores = reliability * repeatability
+    # Y = torch.cat(Y)
+    # X = torch.cat(X)
+    Y = torch.tensor([4 * i for i in range(H//4) for j in range(W//4)], dtype=torch.float32)
+    X = torch.tensor([4 * j for i in range(H//4) for j in range(W//4)], dtype=torch.float32)
+    S = torch.ones_like(X)
+    descriptors = res['descriptors'][0]
+    D = torch.stack([descriptors[0,:,int(Y[i]),int(X[i])] for i in range(len(X))])
+
+    # S = torch.cat(S) # scale
+    # scores = torch.cat(C) * torch.cat(Q) # scores = reliability * repeatability
+    scores = torch.zeros_like(X)
+    print(Y, X)
     XYS = torch.stack([X,Y,S], dim=-1)
-    D = torch.cat(D)
     return XYS, D, scores
 
 
